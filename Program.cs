a698a78eab0b7a798b6863ace5080551fce77a0c
Lucas Souza <90305152+lucasbenacci@users.noreply.github.com>
2024-04-17 17:22:58 -0300

    atualização

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using Modulo2_aulas.Models;
 using Modulo2BootcampDotnet.Models;
+//Troco a configuração do sistema para a localização desejada.
+using System.Globalization;
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-us");
 
 //instanciando a classe Pessoa
 //Não é necessário indicar o parametro, porém facilita a visualização.
@@ -19,6 +22,20 @@ cursoDeIngles.AdicionarAluno(p2);
 
 cursoDeIngles.ListarAluno();
 
+Console.WriteLine("---------------------------------------");
 
+string n1 = "10";
+string n2 = "20";
+
+string result = n1 + n2;
+
+Console.WriteLine(result);
+
+Console.WriteLine("---------------------------------------");
+
+decimal valorMonetario = 82.40M;
+
+//:C formata de acordo com a configuração local, c = currency 
+Console.WriteLine($"{valorMonetario:C}");
 
 
